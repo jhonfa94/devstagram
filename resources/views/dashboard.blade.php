d12ebfd3ed89bf36c01751b4996ddcbd76ab3526
Jhon Fabio Cardona Martinez <fabio.cardona@coytex.com.co>
2022-06-04 12:39:31 -0500

    Introduccion a los componentes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -34,7 +34,7 @@
 
                 <p class="text-gray-700 text-sm mb-3 font-bold mt-5">
                     {{ $user->followers->count() }}
-                    <span class="font-normal">@choice('Seguidor|Seguidores',$user->followers->count())</span>
+                    <span class="font-normal">@choice('Seguidor|Seguidores', $user->followers->count())</span>
                 </p>
 
                 <p class="text-gray-700 text-sm mb-3 font-bold">
@@ -75,21 +75,6 @@
 
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
-        @if (count($posts) > 0)
-            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grap-6">
-                @foreach ($posts as $post)
-                    <div class="">
-                        <a href="{{ route('post.show', ['user' => $post->user, 'post' => $post]) }}">
-                            <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="{{ $post->titulo }}">
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-            <div class="mt-10">
-                {{ $posts->links('pagination::tailwind') }}
-            </div>
-        @else
-            <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
-        @endif
+        <x-listar-post :posts="$posts" />
     </section>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,20 +5,5 @@
 @endsection
 
 @section('contenido')
-    @if ($posts->count() > 0)
-       <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grap-6">
-                @foreach ($posts as $post)
-                    <div class="">
-                        <a href="{{ route('post.show', ['user' => $post->user, 'post' => $post]) }}">
-                            <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="{{ $post->titulo }}">
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-            <div class="mt-10">
-                {{ $posts->links('pagination::tailwind') }}
-            </div>
-    @else
-        <p class="text-center">No hay Posts, sugue a alguien para poder mostrar sus posts</p>
-    @endif
+    <x-listar-post :posts="$posts"/>
 @endsection
